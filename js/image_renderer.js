f2639f246ba613fc411fe91c00cda94cfaa64582
Jason Tam <jtam@adparlor.com>
2016-09-09 17:01:47 -0400

    initial implementation

--- js/image_renderer.js
+++ js/image_renderer.js
@@ -1,13 +1,27 @@
 
 (function(){
+  var showFullScreenImage = function(imgSrc) {
+    $("#appOverlay").removeClass("hide")
+    $("#imageLightbox").removeClass("hide")
+
+    $("#imageLightbox #fullsizeImage")[0].src = imgSrc
+  }
+
+  var hideFullScreenImage = function() {
+    $("#appOverlay").addClass("hide")
+    $("#imageLightbox").addClass("hide")
+  }
 
   _.each(imagesList, function(imagePath, index){
     $("#appContent").append("<img id=\"image"+index+"\""+"src=\""+imagePath+"\"/>")
-    debugger
 
     $('body').on('click', "#image"+index, function(){
-      console.log("fuck")
+      showFullScreenImage(imagePath)
     })
+  })
+
+  $("#appOverlay, #imageLightbox").click(function(){
+    console.log("fuck")
   })
 
 })();
